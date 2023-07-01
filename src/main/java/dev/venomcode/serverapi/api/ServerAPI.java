66e0e1f43b78b2320ab850e5e2ebd1904546f482
VenomCode <103589496+VenomCodeDev@users.noreply.github.com>
2023-06-30 19:35:54 -0500

    Fixed ServerAPI.CONFIG_PATH to use forward slashes instead of backslashes.

--- src/main/java/dev/venomcode/serverapi/api/ServerAPI.java
+++ src/main/java/dev/venomcode/serverapi/api/ServerAPI.java
@@ -22,7 +22,7 @@ public final class ServerAPI {
         return null;
     }
 
-    public static final String CONFIG_PATH = ".\\config\\";
+    public static final String CONFIG_PATH = "./config/";
 
     public final static class Logger
     {
